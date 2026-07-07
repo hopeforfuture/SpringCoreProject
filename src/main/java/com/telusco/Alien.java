dc0de88e3af61a6ffbe8ef7a42cc8412458e8657
hopeforfuture <mnbl87@gmail.com>
2026-07-07 19:30:27 +0530

    exploring autowire functionality in spring

--- src/main/java/com/telusco/Alien.java
+++ src/main/java/com/telusco/Alien.java
@@ -3,16 +3,16 @@ package com.telusco;
 public class Alien {
 
     private int age;
-    private Laptop lap;
+    private Computer comp;
 
     public Alien() {
         System.out.println("Alien Object Created");
     }
 
-    public Alien(int age, Laptop lap) {
+    public Alien(int age, Computer comp) {
         System.out.println("constructor calling for alien obj3");
         this.age = age;
-        this.lap = lap;
+        this.comp = comp;
     }
 
     public int getAge() {
@@ -23,17 +23,16 @@ public class Alien {
         this.age = age;
     }
 
-    public Laptop getLap() {
-        return lap;
+    public Computer getComp() {
+        return comp;
     }
 
-    public void setLap(Laptop lap) {
-        this.lap = lap;
+    public void setComp(Computer comp) {
+        this.comp = comp;
     }
-
 
     public void code() {
         System.out.println("Coding...");
-        lap.compile();
+        comp.compile();
     }
 }
