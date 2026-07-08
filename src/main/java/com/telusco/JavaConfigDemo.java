1f2b1707f4b8e22328be0ccfcf275f242334c2e8
hopeforfuture <mnbl87@gmail.com>
2026-07-08 16:58:07 +0530

    exploring Qualifier and Primary in spring framework

--- src/main/java/com/telusco/JavaConfigDemo.java
+++ src/main/java/com/telusco/JavaConfigDemo.java
@@ -7,7 +7,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class JavaConfigDemo {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Alien obj1 = context.getBean(Alien.class);
+        System.out.println(obj1.getAge());
+        obj1.code();
+        System.out.println("---------------------------------");
         Desktop dt = context.getBean("desktop", Desktop.class);
         dt.compile();
+
+        //Desktop dt1 = context.getBean("desktop", Desktop.class);
+        //dt1.compile();
     }
 }
--- src/main/java/com/telusco/config/AppConfig.java
+++ src/main/java/com/telusco/config/AppConfig.java
@@ -1,14 +1,35 @@
 package com.telusco.config;
 
+import com.telusco.Alien;
+import com.telusco.Computer;
 import com.telusco.Desktop;
+import com.telusco.Laptop;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean
+    public Alien alien(Computer comp) { //@Qualifier("laptop")
+        Alien obj = new Alien();
+        obj.setAge(25);
+        obj.setComp(comp);
+        return obj;
+    }
+
+    @Bean
+    @Scope("singleton")
     public Desktop desktop() {
         return new Desktop();
+    }
+
+    @Bean
+    @Primary
+    public Laptop laptop() {
+        return new Laptop();
     }
 }
